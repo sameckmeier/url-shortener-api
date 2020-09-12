4bf94498037d4882a89cdf0af1f4a779865055ac
Sam Eckmeier <eckmeier41@gmail.com>
2020-09-12 16:09:46 -0400

    adds routes for urls, client_token and clients

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/:shortened_url', to: 'api/v1/urls#show'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      delete '/urls/:shortened_url', to: 'urls#destroy'
+      
+      resources :urls, only: [:create]
+      resources :client_token, only: [:create]
+      resources :clients, only: [:create]
+    end
+  end
 end
