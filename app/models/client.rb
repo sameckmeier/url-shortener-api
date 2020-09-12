b872e00176d8d2ce1324732eef965679d25be78b
Sam Eckmeier <eckmeier41@gmail.com>
2020-09-12 16:12:46 -0400

    overrides knock method in client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,16 @@
+require 'bcrypt'
+
 class Client < ApplicationRecord
   self.implicit_order_column = :created_at
 
   has_secure_password :secret
 
   has_many :urls, dependent: :destroy
+
+  class << self
+    def from_token_request(request)
+      client_id = request.params['auth'] && request.params['auth']['client_id']
+      find_by(id: client_id)
+    end
+  end
 end
