1b375fc79e2d6b520dd548dde18e6cc9d13ea80a
Sam Eckmeier <eckmeier41@gmail.com>
2020-09-12 16:13:19 -0400

    implements ShortenedUrl#increment_sequence

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -5,5 +5,37 @@ class ShortenedUrl < ApplicationRecord
     alias current first
   end
 
-  def increment_sequence; end
+  def increment_sequence
+    chars = sequence.split('')
+    i = chars.length - 1
+
+    if chars[i] == final_char
+      while i >= 0 && chars[i] == final_char
+        chars[i] = next_char(chars[i])
+        i -= 1
+      end
+
+      chars << initial_char if i == - 1
+    end
+
+    chars[i] = next_char(chars[i]) if i > -1
+
+    update!(sequence: chars.join(''))
+
+    self
+  end
+
+  private
+
+  def initial_char
+    'a'
+  end
+
+  def final_char
+    '9'
+  end
+
+  def next_char(char)
+    char.tr('a-zA-Z0-9', 'b-zA-Z0-9a')
+  end
 end
--- spec/models/shortened_url_spec.rb
+++ spec/models/shortened_url_spec.rb
@@ -10,10 +10,48 @@ describe ShortenedUrl do
   end
 
   describe :current do
-    pending "TODO"
+    let!(:shortened_url) { create(:shortened_url) }
+
+    it 'returns first ShortenedUrl' do
+      expect(ShortenedUrl.current).to eq(shortened_url)
+    end
   end
 
   describe :increment_sequence do
-    pending "TODO"
+    subject(:shortened_url) { create(:shortened_url, sequence: initial_char).increment_sequence }
+
+    context 'when last char is not the final char' do
+      let(:initial_char) { 'a' }
+
+      it 'increments the final char' do
+        expect(shortened_url.sequence).to eq('b')
+      end
+    end
+
+    context 'when last char is the final char' do
+      context 'and it is in the first position' do
+        let(:initial_char) { '9' }
+
+        it 'resets it to the initial char and appends a char to the sequence' do
+          expect(shortened_url.sequence).to eq('aa')
+        end
+      end
+
+      context 'and the char previous to it is not the final char' do
+        let(:initial_char) { 'a9' }
+
+        it 'resets it to the initial char and increments the previous char' do
+          expect(shortened_url.sequence).to eq('ba')
+        end
+      end
+
+      context 'and the char previous to it is the final char' do
+        let(:initial_char) { '99' }
+
+        it 'resets it to the initial char and increments the previous char' do
+          expect(shortened_url.sequence).to eq('aaa')
+        end
+      end
+    end
   end
 end
