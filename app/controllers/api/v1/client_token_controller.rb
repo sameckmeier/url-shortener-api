e1c8822ae5327067a7fe364a385292de2d44b0b3
Sam Eckmeier <eckmeier41@gmail.com>
2020-09-16 07:28:26 -0400

    adds comments regarding overriding Knock methods

--- app/controllers/api/v1/client_token_controller.rb
+++ app/controllers/api/v1/client_token_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ClientTokenController < Knock::AuthTokenController
   private
 
+  # Below methods override ones defined by Knock gem: https://github.com/nsarno/knock/blob/master/app/controllers/knock/auth_token_controller.rb
   def authenticate
     raise Knock.not_found_exception_class unless entity.present? && entity.authenticate_secret(auth_params[:secret])
   end
--- app/models/client.rb
+++ app/models/client.rb
@@ -8,6 +8,7 @@ class Client < ApplicationRecord
   has_many :urls, dependent: :destroy
 
   class << self
+    # Overrides method defined by Knock gem: https://github.com/nsarno/knock#in-the-entity-model
     def from_token_request(request)
       client_id = request.params['auth'] && request.params['auth']['client_id']
       find_by(id: client_id)
